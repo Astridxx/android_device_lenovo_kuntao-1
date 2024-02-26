afe4615e6c61b2f27d1ab92304018f4f85acf292
Astridxx <muratkozan350@gmail.com>
2024-02-26 14:08:21 +0300

    kuntao: Add Dolby Atmos support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,9 @@ rm -rf vendor/lenovo && git clone --depth=1 https://github.com/Astridxx/propriet
 echo 'Cloning revamped fmradio'
 rm -rf packages/apps/RevampedFMRadio && git clone --depth=1 https://github.com/iusmac/RevampedFMRadio -b qcom packages/apps/RevampedFMRadio
 
+echo 'Cloning dolby atmos'
+git clone --depth=1 https://github.com/Astridxx/vendor_dolby -b lineage-21.0 vendor/dolby
+
 echo 'Cloning process completed, Patching process started...'
 
 echo 'Adding patch to Bionic'
@@ -80,7 +83,15 @@ cd ../..
 echo 'Adding patch to Frameworks av'
 cd frameworks/av
 wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0001-Restore-clearkey-hidl-code.patch
+wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0002-OMXStore-Import-loading-libstagefrightdolby.patch
+wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0003-Import-Dolby-Effects-initialization.patch
+wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0004-Add-support-for-loading-prebuilt-ddp-decoder-lib.patch
+wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0005-Do-not-allow-DAP-effect-to-be-suspended.patch
 patch -p1 <0001-Restore-clearkey-hidl-code.patch
+patch -p1 <0002-OMXStore-Import-loading-libstagefrightdolby.patch
+patch -p1 <0003-Import-Dolby-Effects-initialization.patch
+patch -p1 <0004-Add-support-for-loading-prebuilt-ddp-decoder-lib.patch
+patch -p1 <0005-Do-not-allow-DAP-effect-to-be-suspended.patch
 cd ../..
 
 echo 'Adding patch to Frameworks net'
